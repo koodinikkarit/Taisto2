edc7b23d7b8597650613e65ac5a9dac856886f63
Jaska <ei.kuulu@sulle.t>
2017-01-08 16:39:24 +0200

    Lisätty uusia routeja.

--- Taisto/js/routes.js
+++ Taisto/js/routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Route} from "react-router";
+import {Route, IndexRoute} from "react-router";
 
 import Diagram from "./containers/Diagram";
 import Etusivu from "./containers/Etusivu";
@@ -10,7 +10,12 @@ import NotFound from "./components/404";
 export default (
 	<Route>
 		<Route path="/diagram/:slug" component={Diagram} />
-		<Route path="/promode" component={Promode} />
+		<Route path="/promode" component={Promode}>
+			<Route path="/promode/:slug" component={Promode}>
+				<Route path="/promode/:slug/:mode" component={Promode} />
+				<Route path="/promode/:slug/:mode" component={Promode} />
+			</Route>
+		</Route>
 		<Route path="/settings" component={Settings} />
 		<Route path="/"  component={Etusivu}/>
 		<Route path="*" component={NotFound} />
