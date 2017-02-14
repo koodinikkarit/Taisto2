8935c013b75f4363b5ab7e5e0ad0ce22d3aebca4
Jaska <pusanteemu@gmail.com>
2017-02-14 22:39:03 +0200

    Vaihdettu composeen.

--- js/containers/MatrixSettings.js
+++ js/containers/MatrixSettings.js
@@ -1,54 +1,58 @@
 import React from 'react';
-import { graphql } from 'react-apollo';
+import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 
 import MatrixSettings from "../components/MatrixSettings";
 
-export default graphql(gql`
-query matrixs($slug: String!) {
-    matrix(slug: $slug) {
-        id
-        slug
-        ip
-        port
-        conPorts {
-            id
-            slug
-            portNum
-        }
-        cpuPorts {
-            id
-            slug
-            portNum
-        }
-    }
-}
-`, {
-    options: (ownProps) =>  {
-        return {
-            variables: {
-                slug: ownProps.params.slug
-            }
-        }
-    },
-    props: ({ ownProps, data: { matrix } }) => ({
-        matrix
-    })
-})(graphql(gql`
-mutation editMatrix($id: String!, $slug: String, $ip: String, $port: Int) {
-    editMatrix(id: $id, slug: $slug, ip: $ip, port: $port) {
-        slug
-    }
-}`, { name: "editMatrixMutation" })(graphql(gql`
-mutation ediConPort($id: String!, $slug: String){
-    editConPort(id: $id, slug: $slug) {
-        id
-        slug
-    }
-}`, { name: "editConPortMutation" })(graphql(gql`
-mutation editCpuPort($id: String!, $slug: String){
-    editCpuPort(id: $id, slug: $slug) {
-        id
-        slug
-    }
-}`, { name: "editCpuPortMutation" })(MatrixSettings))));
+export default compose(
+	graphql(gql`
+	query matrixBySlug($slug: String!) {
+    	matrix: matrixBySlug(slug: $slug) {
+        	id
+        	slug
+        	ip
+        	port
+        	conPorts {
+        	    id
+        	    slug
+        	    portNum
+        	}
+        	cpuPorts {
+        	    id
+        	    slug
+        	    portNum
+        	}
+    	}
+	}`, {
+		options: (ownProps) => {
+			return {
+				variables: {
+					slug: ownProps.params.slug
+				}
+			}
+		},
+		props: ({ ownProps, data: { matrix } }) => ({
+			matrix
+		})
+	}),
+	graphql(gql`
+	mutation editMatrix($id: String!, $slug: String, $ip: String, $port: Int) {
+    	editMatrix(id: $id, slug: $slug, ip: $ip, port: $port) {
+     	   slug
+    	}
+	}`, { name: "editMatrixMutation" }),
+	graphql(gql`
+	mutation ediConPort($id: String!, $slug: String){
+    	editConPort(id: $id, slug: $slug) {
+        	id
+        	slug
+    	}
+	}`, { name: "editConPortMutation" }),
+	graphql(gql`
+	mutation editCpuPort($id: String!, $slug: String){
+    	editCpuPort(id: $id, slug: $slug) {
+        	id
+        	slug
+    	}
+	}`, { name: "editCpuPortMutation" })
+)(MatrixSettings);
